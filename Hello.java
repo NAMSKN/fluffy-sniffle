6648a95333a3c0568d414e9c639eb2c1d78247b0
NAMSKN <namskn8@gmail.com>
2021-11-10 20:09:33 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
 import java.io.*;
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        int a = 10, b = 10;
+        int res;
+        res = a + b;
+        System.out.println("Addition of "+a+" and "+b+" is "+res);
+        System.out.println(" Let's try out changing things for good !!");
     }
 }
